f758afde63c1db4d42ad7fd4d062c8908876fc7c
nagarjun-ME <nagarjun.mqdev08@gmail.com>
2020-11-22 00:52:50 -0500

    created Eureka service manager

--- DBService/src/main/java/com/naga/spring/dbservice/DbServiceApplication.java
+++ DBService/src/main/java/com/naga/spring/dbservice/DbServiceApplication.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DbServiceApplication {
 
 	private Logger log= LoggerFactory.getLogger(this.getClass());
